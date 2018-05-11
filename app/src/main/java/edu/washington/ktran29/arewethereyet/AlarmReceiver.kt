bf473f303ee61afe1933c60ebbfed848ba0fdb89
Kevin Tran <ktran@tack.tech>
2018-05-11 16:13:11 -0700

    finish basic functionality

--- app/src/main/java/edu/washington/ktran29/arewethereyet/AlarmReceiver.kt
+++ app/src/main/java/edu/washington/ktran29/arewethereyet/AlarmReceiver.kt
@@ -3,13 +3,19 @@ package edu.washington.ktran29.arewethereyet
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.widget.Toast
+
+private const val TAG = "AlarmReceiver"
 
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
+        Log.i(TAG, "onReceive Started")
+
         val messageText = intent?.getStringExtra("message")
         var phoneNumber = intent?.getStringExtra("number")
         phoneNumber = "(${phoneNumber?.substring(0, 3)}) ${phoneNumber?.substring(3, 6)} ${phoneNumber?.substring(6)}"
         Toast.makeText(context, "$phoneNumber: $messageText", Toast.LENGTH_SHORT).show()
+        Log.i(TAG, "Toast Created")
     }
 }
--- app/src/main/java/edu/washington/ktran29/arewethereyet/MainActivity.kt
+++ app/src/main/java/edu/washington/ktran29/arewethereyet/MainActivity.kt
@@ -1,8 +1,16 @@
 package edu.washington.ktran29.arewethereyet
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,18 +18,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val messageText = message
-        val phoneNumber = number
-        val nagInterval = interval
+        val messageText = message.text
+        val phoneNumber = number.text
+        val nagInterval = interval.text
 
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, AlarmReceiver::class.java)
+        
         button.setOnClickListener {
             if (button.text == "Start") {
-                if (messageText.text.isNotEmpty() && phoneNumber.length() == 10 && nagInterval.text.isNotEmpty()) {
+                if (messageText.isNotEmpty() && phoneNumber.length == 10 && nagInterval.isNotEmpty()) {
+                    Log.i(TAG, "Alarm Started")
                     button.text = "Stop"
+                    intent.putExtra("message", messageText.toString())
+                    intent.putExtra("number", phoneNumber.toString())
+//                    Log.i(TAG, "$messageText text")
+
+                    val alarmInterval = nagInterval.toString().toLong() * 1000 * 60
+                    val alarmIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+                    alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), alarmInterval, alarmIntent)
                 }
 
             } else {
+                Log.i(TAG, "Alarm Stopped")
                 button.text = "Start"
+                val alarmIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT)
+                alarmManager.cancel(alarmIntent)
             }
         }
 
