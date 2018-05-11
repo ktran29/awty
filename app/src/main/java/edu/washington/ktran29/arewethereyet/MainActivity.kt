bf29cd2e2a8a14b5cd5ad4f186673063f60ee89d
Kevin Tran <ktran@tack.tech>
2018-05-11 14:58:24 -0700

    add some starter code

--- app/src/main/java/edu/washington/ktran29/arewethereyet/MainActivity.kt
+++ app/src/main/java/edu/washington/ktran29/arewethereyet/MainActivity.kt
@@ -2,11 +2,28 @@ package edu.washington.ktran29.arewethereyet
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val messageText = message
+        val phoneNumber = number
+        val nagInterval = interval
+
+        button.setOnClickListener {
+            if (button.text == "Start") {
+                if (messageText.text.isNotEmpty() && phoneNumber.length() == 10 && nagInterval.text.isNotEmpty()) {
+                    button.text = "Stop"
+                }
+
+            } else {
+                button.text = "Start"
+            }
+        }
+
     }
 }
