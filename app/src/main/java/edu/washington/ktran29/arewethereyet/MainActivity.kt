d5b3313bd041079b5618b3b6b8e3683a8176c78d
Kevin Tran <ktran@tack.tech>
2018-05-11 17:35:46 -0700

    create descriptive failure toasts

--- app/src/main/java/edu/washington/ktran29/arewethereyet/MainActivity.kt
+++ app/src/main/java/edu/washington/ktran29/arewethereyet/MainActivity.kt
@@ -7,8 +7,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 private const val TAG = "MainActivity"
 
@@ -24,20 +24,33 @@ class MainActivity : AppCompatActivity() {
 
         val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
         val intent = Intent(this, AlarmReceiver::class.java)
-        
+
         button.setOnClickListener {
             if (button.text == "Start") {
-                if (messageText.isNotEmpty() && phoneNumber.length == 10 && nagInterval.isNotEmpty()) {
+//                Log.i(TAG, "${nagInterval.toString().toLong()}")
+                if (messageText.isNotEmpty() && phoneNumber.length == 10 && (nagInterval.isNotEmpty() && nagInterval.toString() != "0")) {
                     Log.i(TAG, "Alarm Started")
                     button.text = "Stop"
                     intent.putExtra("message", messageText.toString())
                     intent.putExtra("number", phoneNumber.toString())
-//                    Log.i(TAG, "$messageText text")
 
                     val alarmInterval = nagInterval.toString().toLong() * 1000 * 60
                     val alarmIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
                     alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), alarmInterval, alarmIntent)
+                } else {
+                    if (messageText.isEmpty()) {
+                        Toast.makeText(this, "Message required", Toast.LENGTH_SHORT).show()
+                    }
+                    if (phoneNumber.length != 10 || phoneNumber.isEmpty()) {
+                        Toast.makeText(this, "Valid phone number required", Toast.LENGTH_SHORT).show()
+                    }
+                    if (nagInterval.isEmpty()) {
+                        Toast.makeText(this, "Interval required", Toast.LENGTH_SHORT).show()
+                    } else if (nagInterval.toString() == "0") {
+                        Toast.makeText(this, "Interval cannot be zero", Toast.LENGTH_SHORT).show()
+                    }
+                    Log.i(TAG, "Improperly formatted toasts sent")
                 }
 
             } else {
